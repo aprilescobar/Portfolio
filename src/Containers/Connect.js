e4beef8a0781168242b5375d374cccc16ca41a39
aprilfaithdays <aprilfaith.e@gmail.com>
2020-07-23 23:21:11 -0500

    added email functionality

--- src/Containers/Connect.js
+++ src/Containers/Connect.js
@@ -8,6 +8,7 @@ const Connect = () => {
                     <h1>Let's Connect!</h1>
                 </div>
                 <div className="center">
+                    <a className="color" href="mailto: aprilfaith.e@gmail.com">Email</a> | {' '}
                     <a className="color" href="https://www.linkedin.com/in/april-escobar/" target="_blank" rel="noopener noreferrer">LinkedIn</a> | {' '}
                     <a className="color" href="https://github.com/aprilfaithdays" target="_blank" rel="noopener noreferrer">Github</a>
                 </div>
